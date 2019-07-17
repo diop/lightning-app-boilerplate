2856112dfd7ff8cacfb835762defd5465c48619f
diop <fodesdiop@gmail.com>
2019-07-17 08:53:31 -0700

    add and hide credentials

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lightning' });
-});
+  res.render('index', { title: 'Lightning', message: "Welcome to Litrides" })
+})
 
-module.exports = router;
+module.exports = router
--- routes/invoice.js
+++ routes/invoice.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var router = express.Router()
+require('dotenv').config()
+const express = require('express')
+const router = express.Router()
 const BTCPAY_PRIV_KEY = process.env.BTCPAY_PRIV_KEY
 const BTCPAY_MERCHANT_KEY = process.env.BTCPAY_MERCHANT_KEY
 
@@ -8,7 +9,6 @@ const btcpay = require('btcpay')
 const keypair = btcpay.crypto.load_keypair(new Buffer.from(BTCPAY_PRIV_KEY, 'hex'))
 const client = new btcpay.BTCPayClient('https://btcpay.okayrelay.com', keypair, {merchant: BTCPAY_MERCHANT_KEY})
 
-
 /* get & verify invoice. */
 router.get('/:id', async function(req, res, next) {
 
@@ -16,7 +16,17 @@ router.get('/:id', async function(req, res, next) {
 
 /* Create invoice. */
 router.post('/', function(req, res, next) {
-
+    const dollarAmount = req.body.amount
+    console.log(dollarAmount)
+    client.create_invoice({
+        price: dollarAmount,
+        currency: 'USD'
+    })
+    .then(invoice => {
+        console.log(invoice)
+        res.render('invoice', {invoiceId: invoice.id})
+    })
+    .catch(err => console.log(err))
 })
 
 
